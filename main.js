37350b5f98db698b4274807a80a74970a8f3d2e2
japoon999 <aziz.azizli.95@inbox.ru>
2023-11-13 16:55:33 +0400

    straighten

--- main.js
+++ main.js
@@ -35,16 +35,22 @@ function calculateChildAllowance(numChildren) {
     }
 }
 
-function calculateTaxRate(grossSalary) {
+function calculateTaxRate(grossSalary, isDisabled) {
+    let taxRate;
     if (grossSalary <= 1000) {
-        return 0.15;
+        taxRate = 0.15;
     } else if (1000 < grossSalary && grossSalary <= 2000) {
-        return 0.2;
+        taxRate = 0.2;
     } else if (2000 < grossSalary && grossSalary <= 3000) {
-        return 0.25;
+        taxRate = 0.25;
     } else {
-        return 0.3;
+        taxRate = 0.3;
     }
+    if (isDisabled.toLowerCase() === 'beli') {
+        taxRate *= 0.5;
+    }
+    return taxRate;
+
 }
 
 function calculateIncomeTax(grossSalary, taxRate) {
@@ -52,16 +58,16 @@ function calculateIncomeTax(grossSalary, taxRate) {
 }
 
 function calculateSalary() {
-    const grossSalary = prompt("Brüt əmək haqqını daxil edin:");
+    const grossSalary = parseFloat(prompt("Brüt əmək haqqını daxil edin:"));
     const maritalStatus = prompt("Ailə vəziyyətinizi daxil edin (evli, subay, dul):");
-    const numChildren = prompt("Uşaq sayını daxil edin:");
+    const numChildren = parseInt(prompt("Uşaq sayını daxil edin:"));
     const isDisabled = prompt("Əlil olub-olmadığınızı daxil edin (beli, xeyir):");
 
     const familyAllowance = calculateFamilyAllowance(maritalStatus, numChildren);
-    const taxRate = calculateTaxRate(grossSalary);
-    const incomeTax = calculateIncomeTax(grossSalary, isDisabled.toLowerCase() === 'beli' ? taxRate * 0.5 : taxRate);
+    const taxRate = calculateTaxRate(grossSalary, isDisabled);
+    const incomeTax = calculateIncomeTax(grossSalary, taxRate);
 
-    let totalSalary = parseFloat(grossSalary) + familyAllowance;
+    let totalSalary = grossSalary + familyAllowance;
     let netSalary = totalSalary - incomeTax;
 
     const denominations = [200, 100, 50, 20, 10, 5, 1];
@@ -74,12 +80,12 @@ function calculateSalary() {
     alert(
         "Ailə müavinəti: " + familyAllowance +
         "\n Uşaq pulu: " + familyAllowance +
-        "\n Gəlir vergisi dərəcəsi: " + (taxRate * 100).toFixed(2) + "%" +
-        "\n Gəlir vergisinin məbləği: " + incomeTax.toFixed(2) + " AZN" +
-        "\n Ümumi əmək haqqı: " + totalSalary.toFixed(2) + " AZN" +
-        "\n Xalis əmək haqqı: " + netSalary.toFixed(2) + " AZN" +
+        "\n Gəlir vergisi dərəcəsi: " + (taxRate * 100) + "%" +
+        "\n Gəlir vergisinin məbləği: " + incomeTax + " AZN" +
+        "\n Ümumi əmək haqqı: " + totalSalary + " AZN" +
+        "\n Xalis əmək haqqı: " + netSalary + " AZN" +
         "\n* Pul vahidləri:\n" +
-        denominations.map((d, index) => `   * ${d} AZN: ${denominationCount[index]} vahid`).join("\n")
+        denominations.map((d, index) => `  * ${d} AZN: ${denominationCount[index]} vahid`).join("\n")
     );
 }
 
